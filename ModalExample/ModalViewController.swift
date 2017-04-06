687da8c8f93c7b895054a181cf7851fa49829704
Tito Albino Evangelista da Silva Junior <titoaesj@gmail.com>
2017-04-05 22:09:08 -0300

    Criando modal com UIViewController

--- ModalExample/ModalViewController.swift
+++ ModalExample/ModalViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ModalViewController.swift
 //  ModalExample
 //
 //  Created by Tito Albino Evangelista da Silva Junior on 05/04/17.
@@ -8,35 +8,11 @@
 
 import UIKit
 
-class ModalViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    @IBOutlet weak var mListTableView: UITableView!
-    
-    /*:
-        Dados para popular o UITableView
-     */
-    let source : [(index: Int, title: String)] = [
-        (index: 0, title: "Modal com UIViewController"),
-         (index: 1, title: "Modal com UIView")]
+class ModalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.mListTableView.dataSource = self
-        self.mListTableView.delegate = self
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return source.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ItemTableViewCell
-        
-        cell.entity = source[indexPath.row]
-        
-        return cell
-    }
 
+    }
+    
 }
-
